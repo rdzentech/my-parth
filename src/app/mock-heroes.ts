0f3b0c9cf295587fbe64c2c4d086d3c3ff4532ae
rdzentech <32627505+rdzentech@users.noreply.github.com>
2020-02-24 01:24:40 +0530

    Push the changes to new Repo My-parth

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES:Hero[]=
+[
+    {id:11,name:'Dr Nice'},
+    {id:12,name:'Diksha'},
+    {id:13,name:'Rana'},
+    {id:14,name:'Duggu'},
+    {id:15,name:'Anannya'},
+    {id:16,name:'Mangal'},
+    {id:17,name:'Bittu'},
+    {id:18,name:'Gunnu'},
+    {id:19,name:'Soumya'},
+    {id:20,name:'Rahul'},
+    {id:20,name:'GunGUN'}
+];
